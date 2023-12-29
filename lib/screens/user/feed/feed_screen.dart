23c49d3b3212b4a976abdd7dd4d1907ade35581f
stevenwj12 <stevenwilbertjamin@gmail.com>
2023-12-29 14:03:25 +0700

    add content based filtering

--- lib/screens/user/feed/feed_screen.dart
+++ lib/screens/user/feed/feed_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:tuktraapp/services/user_service.dart';
 import 'package:tuktraapp/widgets/post_card.dart';
 
 class FeedScreen extends StatefulWidget {
@@ -27,46 +28,102 @@ class _FeedScreenState extends State<FeedScreen> {
         body: SafeArea(
           child: RefreshIndicator(
               onRefresh: _pullRefresh,
-              child: StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('feeds')
-                      .snapshots(),
-                  builder: (context,
-                      AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
-                          snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
+              child: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                future: FirebaseFirestore.instance.collection('feeds').get(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return const Center(child: Text('No data available'));
+                  }
+
+                  var likedTags = <String>[];
+                  for (var doc in snapshot.data!.docs) {
+                    var likes = List<Map<String, dynamic>>.from(doc['likes']);
+                    if (likes.any((like) =>
+                        like['userId'] == UserService().currUser!.uid)) {
+                      var tags = List<String>.from(
+                          doc['tags'].map((tag) => tag.toLowerCase()));
+                      for (var tag in tags) {
+                        likedTags.add(tag);
+                      }
+                    }
+                  }
+
+                  // Count the occurrences of each tag in likedTags
+                  var tagOccurrences = <String, int>{};
+                  for (var tag in likedTags) {
+                    tagOccurrences[tag] = (tagOccurrences[tag] ?? 0) + 1;
+                  }
+
+                  var feeds = snapshot.data!.docs.map((doc) {
+                    var tags = List<String>.from(doc['tags']);
+                    var likedTagsCount = tags.fold(
+                        0,
+                        (count, tag) =>
+                            count + (tagOccurrences[tag.toLowerCase()] ?? 0));
+
+                    return {
+                      'originalDoc': doc.data(),
+                      'likedTagsCount': likedTagsCount,
+                    };
+                  }).toList();
+
+                  feeds.sort((a, b) {
+                    var likedTagsCountA = a['likedTagsCount'] as int;
+                    var likedTagsCountB = b['likedTagsCount'] as int;
+
+                    if (likedTagsCountA != likedTagsCountB) {
+                      return likedTagsCountB.compareTo(
+                          likedTagsCountA); // Sort by likedTagsCount descending
                     }
 
-                    if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
+                    var userLikedFeedA = likedTags.contains(((a['originalDoc']
+                            as Map<String, dynamic>?)?['feedId'] as String?) ??
+                        '');
+                    var userLikedFeedB = likedTags.contains(((b['originalDoc']
+                            as Map<String, dynamic>?)?['feedId'] as String?) ??
+                        '');
+
+                    if (userLikedFeedA && !userLikedFeedB) {
+                      return 1; // Feed A comes after Feed B
+                    } else if (!userLikedFeedA && userLikedFeedB) {
+                      return -1; // Feed A comes before Feed B
                     }
 
-                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                      return const Center(child: Text('No data available'));
-                    }
+                    return 0; // Both feeds are either liked or not liked, maintain their order based on tag occurrence
+                  });
 
-                    return !snapshot.hasData
-                        ? const Center(child: Text('No Feed yet'))
-                        : SizedBox.expand(
-                            child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 20.0),
-                                child: ListView.builder(
-                                  physics:
-                                      const AlwaysScrollableScrollPhysics(),
-                                  itemCount: snapshot.data!.docs.length,
-                                  itemBuilder: (ctx, index) => Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: 20.0),
-                                    child: PostCard(
-                                      snap: snapshot.data!.docs[index].data(),
-                                    ),
-                                  ),
-                                )));
-                  })),
+                  return !snapshot.hasData
+                      ? const Center(child: Text('No Feed yet'))
+                      : SizedBox.expand(
+                          child: Container(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 20.0),
+                            child: ListView.builder(
+                              physics: const AlwaysScrollableScrollPhysics(),
+                              itemCount: feeds.length,
+                              itemBuilder: (ctx, index) => Container(
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: 20.0),
+                                child: PostCard(
+                                  snap: feeds[feeds.length - 1 - index]
+                                      ['originalDoc'], // Use the sorted feeds
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                },
+              )),
         ));
   }
 }
--- lib/services/feed_service.dart
+++ lib/services/feed_service.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:tuktraapp/services/user_service.dart';
 
 class FeedService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
